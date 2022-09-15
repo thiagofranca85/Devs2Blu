daeb957d97f70609c51532f89b154a62016fefea
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-09-14 21:59:13 -0300

    Create exercicio.py
    Esboço do exercicio pronto

---
+++ 14-09-2022/exercicio.py
@@ -0,0 +1,39 @@
+# Faça do cadastro dos membros de sua equipe
+
+from time import sleep
+
+border = "-"*5
+livros = []
+
+while True:
+    print(f"{border} Cadastro de Livros {border}\n(1) Cadastrar Livros\n(2) Listar Livros Cadastrados\n(3) Sair")
+    id = input("Opção: ")
+    if not(id.isnumeric()):
+        print("Letras nao são validas como opção!")
+        pass
+    if id == str(1):
+        livro = input("Digite um nome: ")
+        livros.append(livro)
+    elif id == str(2):
+        print(f"{border} Lista de Membros {border}")
+        for i in livros:
+            print(i)
+            sleep(2)
+            
+        print(f"{border}   Fim da Lista   {border}")
+    elif id == str(3):
+        print("Voce Fechou o Programa.")
+        break
+    else:
+        print("Digite uma opção valida")
+
+
+
+
+
+
+
+
+
+
+
