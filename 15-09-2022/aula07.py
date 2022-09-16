f3144ffdcf7c6af30b0f844abf90fb7384591942
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-09-15 21:41:32 -0300

    Comintando tarefa

--- 15-09-2022/aula07.py
+++ 15-09-2022/aula07.py
@@ -1,21 +1,26 @@
 from time import sleep
 
-nome = str(input("Digite seu nome: "))
-sobreNome = str(input("Digite seu sobrenome: "))
-idade = int(input("Digite a sua idade: "))
+media = 0
 
-lista_notas = []
-for lista in range(0, 2):
-    nota = int(input("Digite sua nota: "))
-    lista_notas.append(nota)
+while media < 7:
+    nome = str(input("Digite seu nome: "))
+    sobreNome = str(input("Digite seu sobrenome: "))
+    idade = int(input("Digite a sua idade: "))
+    lista_notas = []
 
-media = sum(lista_notas) / len(lista_notas)
-situacao = 'Reprovado'
+    for lista in range(0, 2):
+        nota = int(input("Digite sua nota: "))
+        lista_notas.append(nota)
 
-if media >= 7:
-    sleep(2)
-    print("*")
-    situacao = "aprovado"
+    media = sum(lista_notas) / len(lista_notas)
+    situacao = 'Reprovado'
 
-dicionario_alunos = {"Nome": nome,"Sobrenome": sobreNome,"Idade": idade,"Situacao": situacao, "Media": media}
-print(f"{dicionario_alunos['Nome']} {dicionario_alunos['Sobrenome']} {dicionario_alunos['Idade']} - {dicionario_alunos['Situacao']} {dicionario_alunos['Media']}")
+    if media >= 7:
+        for i in range(0, 6):
+            sleep(1)
+            print("*")
+        situacao = "aprovado"
+ 
+    dicionario_alunos = {"Nome": nome,"Sobrenome": sobreNome,"Idade": idade,"Situacao": situacao, "Media": media}
+    print(f"{dicionario_alunos['Nome']} {dicionario_alunos['Sobrenome']} {dicionario_alunos['Idade']} - {dicionario_alunos['Situacao']} {dicionario_alunos['Media']}")
+
