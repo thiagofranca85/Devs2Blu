cca57aa09847e94b889a819b71eeacf1136a8995
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-10-25 16:48:20 -0300

    Atualizando Exercicio 2

--- Python Functions/exercícios/leituraestatistica/controller.py
+++ Python Functions/exercícios/leituraestatistica/controller.py
@@ -1,14 +1,27 @@
+# A) Quantas pessoas foram cadastradas; 
+# B) A média de idade do grupo; 
+# C) Uma lista com todas as mulheres; 
+# D) Uma lista com todas as pessoas com idade acima da média.
 
 pessoas = []
 
 def cadastro(nome, sexo, idade):
-    dados = {}
-    dados['nome']=nome
-    dados['sexo']=sexo
-    dados['idade']=idade
+    dados = {
+        'nome': nome, 
+        'sexo': sexo,
+        'idade': idade
+    }
     pessoas.append(dados)
     print(f"\nCadastrado efetuado com sucesso.\n")
-
+    
 def listarEstatistica():
-    for number, pessoa in enumerate(pessoas):
-        print(number + 1, pessoa)
+    for i in pessoas:
+        print(i)
+        idades = sum(pessoas, 'idade')
+        mIdades = idades / (len(pessoas))
+   
+    print(f"Há {len(pessoas)} pessoa(s) cadastrada(s).")
+    print(mIdades)
+
+        
+    
--- Python Functions/exercícios/leituraestatistica/main.py
+++ Python Functions/exercícios/leituraestatistica/main.py
@@ -6,27 +6,33 @@
 # D) Uma lista com todas as pessoas com idade acima da média.
 from controller import *
 
-def menu():
-    
+def menu():    
     while True:
         opcao = input("[1]Cadastrar\n[2]Listar Dados\n[3]Fechar Programa\n:> ")
-
         match opcao:
             case '1':
                 nome = input("Nome: ")
                 while True:
-                    sexo = input("Sexo(m ou f): ")
-                    if sexo != ('m' or 'f'):
-                        print("Digite sua orientação sexual corretamente.")
-                    else:
-                        break
-                while True:
+                    sexo = input("Sexo(m ou f ou nb): ")
+                    match sexo:
+                        case 'm':
+                            sexo='m'
+                            break
+                        case 'f':
+                            sexo='f'
+                            break
+                        case 'nb':
+                            sexo='nb'
+                            break
+                        case _:
+                            print("Digite sua orientação sexual corretamente.")                              
+                while True:                                    
                     idade = input("Idade: ")
                     if not(idade.isnumeric()):
                         print("Digite uma idade válida.")
                     else:
                         break
-                    cadastro(nome,sexo,idade)                
+                cadastro(nome,sexo,idade)                
             case '2':
                 listarEstatistica()
             case '3':
@@ -34,5 +40,4 @@ def menu():
                 break
             case _:
                 print("Digite a opção certa ai jovem.")
-
 menu()
