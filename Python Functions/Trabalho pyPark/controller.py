64172614e041f695fd0944ae6137442ce1375d56
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-11-02 19:29:49 -0300

    Atualizando pyPark dentro do Devs2Blu

--- Python Functions/Trabalho pyPark/controller.py
+++ Python Functions/Trabalho pyPark/controller.py
@@ -3,81 +3,119 @@ import os
 
 # Pega Data e Horário Atual do Sistema
 horaAtual = datetime.now()
-
-def disponivel():
-    os.system('cls')
-    with open('estacionamento.txt') as file:
-        lines = file.readlines()
-    
-    if len(lines)<10:
-        pass
-    else:
-        print("Não há vagas disponíveis no momento!")
-    print(f"Existem vagas disponíveis")
-    
-def entrada():
-    os.system('cls')
-    dados = dict()
-    dados['placa'] = str(input("Placa do Veículo: ")).upper().strip()
-    dados['modelo'] = str(input("Marca/Modelo: ")).upper().strip()
-    dados['cor'] = str(input("Cor do veículo: ")).upper().strip()
-    # Input de Hora e Minutos pra Hora de Entrada
-    EntradaHora = int(input("Digite a hora de entrada: "))
-    EntradaMinuto = int(input("E os minutos: "))
-    # Salva o Horário de Entrada em Horas e Minutos
-    horaEntrada = datetime.combine(date.today(), time(EntradaHora, EntradaMinuto)) 
-    dados['entrada'] = horaEntrada
-    carro = list()
-    carro.append(dados.copy())
-    print(carro)
-    
+  
+    # Salva os dados em um dicionario dentro do arquivo txt.
+def entrada(carro):
     with open('estacionamento.txt', 'a') as arquivo:
         arquivo.write(str(carro)+"\n")
 
+    # Exibe uma tabela simples de preços usando print.
 def tabelaPrecos():
     os.system('cls')
-    print(f"\n{'*'*10} Tabela de Preços {'*'*10}\n\t1a e 2a Horas = R$5\n\tHoras Adicionais = R$2\n")
+    print(f"{'*'*10} Tabela de Preços {'*'*10}\n\t1a e 2a Horas = R$5\n\tHoras Adicionais = R$2\n")
 
+    # Função de saída / Efetuar pagamento
 def saida(placa):
+
+    # Abre o arquivo do estacionamento
     arquivo = open('estacionamento.txt')
 
+    # Variavel de controle caso nao exista um carro com a placa específica
     placaEncontrada = False
     # Verifica se existe a placa no estacionamento e mostra os dados do veiculo
     for linha in arquivo:
-        if placa == eval(linha)['placa']:
-            # Aqui salvar o valor de entrada "datetime" como objeto pra poder fazer o calculo com a horaSaidaAtual no while seguinte.
-            # >> horaEntrada = datetime.eval(linha)['entrada']
+        aux = eval(linha)
+
+        if placa == aux['placa']:
+            # Ao encontrar a placa exibe as todas informaçoes do carro
+            # Salva entradaHora e entradaMinuto como valores inteiros que serão usados ..
+            # .. para o calculo de tempo posteriormente.
+            entradaHora = aux['entradaHora']
+            entradaMinuto = aux['entradaMinuto']          
             placaEncontrada = True
-            relatorio = linha # Customizar informações (Essa linha adiciona é pra customizar o TXT depois com o Nested Replace)
+            # Hora de entrada vinda do txt pelas chaves acima
+            horaEntrada = datetime.combine(date.today(), time(entradaHora, entradaMinuto))
+            # Alterando as informações da linha txt usando NESTED Replace
+            print()
+            relatorio = linha.replace("{'placa':","Placa:").replace("'","").replace("modelo:","Modelo:").replace("cor:","Cor:").replace("entradaHora:","Hora Entrada >").replace(", entradaMinuto:"," :").replace("}","")
             print(relatorio)
-
-        # Se não encontrar a placa mostra a mensagem abaixo
-        if placaEncontrada == False:
-            print("PLACA NÃO ENCONTRADA.")
-
+        
+    # Se não encontrar a placa mostra a mensagem abaixo
+    if placaEncontrada == False:
+        print("\nPLACA NÃO ENCONTRADA.\n")
+        
     while True:
-        opcao = input("[1] Efetuar Pagamento\n[2] Voltar ao Menu")
-        horaSaidaAtual = datetime.combine(date.today(), time(horaAtual.hour, horaAtual.minute))
+        # Salva o Horário de Saída em Horas e Minutos usando o horaAtual.hour e horaAtual.minute como parametros.
+        horaSaidaAtual = datetime.combine(date.today(), time(horaAtual.hour, horaAtual.minute)) 
+    
+        # Opção para efetuar o pagamento e sair ou voltar ao menu.
+        opcao = input("[1] Efetuar Pagamento\n[2] Voltar ao Menu\n>> ")
 
         match opcao:
             case '1':
-                print("Opção de saída")
+                # Calcula a hora de saída.
+                horaSaida = horaSaidaAtual - horaEntrada
+
+                # Transforma as horas em um numero float através dos segundos pra calcular o valor a pagar
+                calcHora = (horaSaida.seconds/60)/60 
+
+                # Calcula o valor das horas e exibe valor e tempo estacionando
+                valor = 0
+                if calcHora < 1:
+                    valor = 5
+                    print(f"\nO carro ficou estacionado por {horaSaida.seconds/60} minutos.")
+                elif calcHora < 2:
+                    valor = 10
+                    print(f"\nO carro ficou estacionado por {(horaSaida.seconds/60)/60:.1f} hora(s).")
+                elif calcHora > 2:
+                    valor = ((round(calcHora) * 2) - 4) + 10
+                    print(f"\nO carro ficou estacionado por {(horaSaida.seconds/60)/60:.1f} horas.")
+                
+                # Imprime o valor do estacionamento
+                print(f"\nValor R${valor:.2f}")
+
+                # Agora vem os comandos pra salvar o index e deletar a linha da placa ao efetuar o pagamento
+                # Abre o arquivo estacionamento
+                arquivo = open('estacionamento.txt')
+
+                # Variavel pra salvar o index que sera salvo a seguir
+                index = 0
+
+                # Aqui procura a placa pela chave e salva o indice da linha encontrada como placaDelete recebendo index
+                for linha in arquivo:
+                    aux = eval(linha)
+
+                    index += 1
+                    
+                    if placa == aux['placa']:
+                        placaDelete = index
+                
+                # Agora que encontramos a pladaDelete fechamos o arquivo.
+                arquivo.close()
+
+                # Abrindo novamente o arquivo, agora usando o with open e salvando as linhas com readlines()
+                with open('estacionamento.txt', 'r') as arquivo:
+                    linhas = arquivo.readlines()
+
+                    # Reabrindo o arquivo agora com o parametro w para re-escrever o txt
+                    with open('estacionamento.txt', 'w') as arquivo:
+
+                        # Variavel criada recebendo valor inteiro igual a 1
+                        posicao = 1
+                        # Estrutura de repetião lendo linhas do arquivo txt
+                        for linha in linhas:
+                            # condicao if se a posicao for diferente da chave
+                            if posicao != placaDelete:
+
+                                # Se a posicao for diferente do indice da placaDelete ele re-escreve o arquivo
+                                arquivo.write(linha)
+                            # Incremento da variavel posicao dentro do FOR
+                            posicao += 1
+
+                    #Print de saida do bloco           
+                    print("Estacionamento Finalizado.\n")
+                    break
             case '2':
                 break
             case _:
-                print("Digite uma opção valida")
-                
-
-
-
-
-            # with open('estacionamento.txt') as arquivo:
-            #     linhas = arquivo.readlines()
-
-            # if placa == eval(linha)['placa']:
-            #     del linhas['placa']
-            
-            # with open('estacionamento.txt', 'w') as arquivo:
-            #     for linha in linhas:
-            #         arquivo.write(linha)        
-
+                print("Selecione uma opção válida.")
--- Python Functions/Trabalho pyPark/main.py
+++ Python Functions/Trabalho pyPark/main.py
@@ -1,4 +1,5 @@
 from controller import entrada, tabelaPrecos, saida
+from time import sleep
 import os
 
 poli = "*" *28
@@ -6,27 +7,46 @@ def menu():
     #print ultilizando polimorfismo e quebra de linhas
     print(f"\n{poli} \nBem-vindo ao Menu do pyPark\n{poli}\n") 
         
-    while True:
-        
+    while True:        
         #variavel com input para escolher uma das opçoes
-        escolha = input("Escolha a Opção Desejada:\n 1 => Entrada de Veículo\n 2 => Tabela de Preços\n 3 => Saída de Veículo\n 4 => Fechamento\n 5 => Encerrar Programa\n>> ") 
+        escolha = input("Escolha a Opção Desejada:\n 1 => Entrada de Veículo\n 2 => Tabela de Preços\n 3 => Saída de Veículo\n 4 => Encerrar Programa\n>> ") 
+        
         match escolha:
             case "1":
-                entrada()
+                os.system('cls')
+                with open('estacionamento.txt') as file:
+                    lines = file.readlines()
+                dispo = 0
+                dispo = 10 - len(lines)
+                if dispo >0:
+                    print(f"Existem {dispo} vagas disponíveis\n")
+                    sleep(0.5)
+                    dados = {}
+                    dados['placa'] = str(input("Placa do Veículo: ")).upper().strip()
+                    dados['modelo'] = str(input("Marca/Modelo: ")).upper().strip()
+                    dados['cor'] = str(input("Cor do veículo: ")).upper().strip()
+                    # Input de Hora e Minutos pra Hora de Entrada
+                    dados['entradaHora'] = int(input("Digite a hora de entrada: "))
+                    dados['entradaMinuto'] = int(input("E os minutos: "))
+                    # Salva o Horário de Entrada em Horas e Minutos
+                    entrada(dados)
+                else:
+                    print("Não há vagas disponíveis no momento!\n")
             case "2":
+                os.system('cls')
+                # Tabela de preços basica exibida com prints.
                 tabelaPrecos()
             case "3":
+                os.system('cls')
                  # Informa a placa e busca no estacionamento.txt
-                placa = input("Digite a placa Ex[ABCD1234] :").strip().upper()
+                placa = input("Digite a placa Ex[ABCD1234]: ").strip().upper()
                 saida(placa)
             case "4":
-                # Fechamento
-                # fechamento()
-                pass
-            case "5":
                 os.system('cls')
+                # Encerra o Programa
                 print("** Programa Encerrado **\n")
                 break
             case _:
                 print("Opção Inválida.")
+
 menu()
