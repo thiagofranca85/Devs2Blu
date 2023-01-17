6d5b2ec33f72360bdbdf158937d82ec79739c46b
Thiago Augusto Franca <thiago.franca@proway.treina>
2023-01-16 22:09:27 -0300

    Adicionando DB e funções Admin e Classe Alunos

--- Django/MoreDevs/alunos/views.py
+++ Django/MoreDevs/alunos/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Alunos
 
+# Dados criados na mão antes do Banco de Dados
 # Create your views here.
+# def index(request):
+#     alunos = {
+#         1:'Thiago',
+#         2:'Jean',
+#         3:'Gisele'
+#     }
+#     dados = {
+#         'nome_do_aluno' : alunos
+#     }
+#     return render(request, 'index.html', dados)
+
+# Código funcionando buscando do Banco de Dados
 def index(request):
-    alunos = {
-        1:'Thiago',
-        2:'Jean',
-        3:'Gisele'
-    }
-    dados = {
-        'nome_do_aluno' : alunos
-    }
+    alunos = Alunos.objects.all()    
+    dados = {'alunos': alunos}
     return render(request, 'index.html', dados)
 
 def aluno(request):
