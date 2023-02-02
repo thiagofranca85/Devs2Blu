45400a816b095f9c114650553be61b7d3e559dcb
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2023-02-01 22:10:02 -0300

    Adicionado simulação DB e novas caracteristicas

--- FastAPI/Aplicacao/main.py
+++ FastAPI/Aplicacao/main.py
@@ -1,28 +1,48 @@
 from fastapi import FastAPI
-from fastapi import HTTPException, status, Path
-from models import Aluno
+from fastapi import HTTPException, status
+from fastapi import Response, Path, Query, Header, Depends
+from typing import Optional, Dict, List, Any
+from models import Aluno, alunos
+from time import sleep
 
-app = FastAPI()
+app = FastAPI(
+    title='MoreDevs2Blu',
+    version='007',
+    description='Desenvolvido pela melhor turma da história',
+        contact={
+        "name": "Thiago",
+        "url": "https://github.com/thiagofranca85",
+        "email": "thiagoaugusto.franca@hotmail.com",
+    }
+)
 
 @app.get('/')
-async def raiz():
+async def root():
     return {"mensagem": "Seja Bem Vindo ao More Devs"}
 
+def db():
+    try:
+        print('conexao com banco')
+        sleep(1)
+    finally:
+        print('conexao com banco')
+        sleep(1)
 
-alunos = {
-    1: {"Nome":"Lirinha", "Idade":19, "E-mail":"Lira@mail.com"},
-    2: {"Nome":"Thiago", "Idade":37, "E-mail":"thiago@mail.com"},
-    3: {"Nome":"Joao", "Idade":17, "E-mail":"joao@mail.com"},
-    4: {"Nome":"Vander", "Idade":41, "E-mail":"Lira@mail.com"},
-
-}
-
-@app.get('/alunos')
+@app.get('/alunos', 
+        description='Lista de todos os Alunos', 
+        summary='Lista de todos os Alunos',
+        response_description='Retorna os Alunos Cadastrados',
+        response_model=List[Aluno]
+        )
 async def get_alunos():
     return alunos
 
 # Lista o Aluno por ID (Sem salvar o ID consultado)
-# @app.get("/alunos/{aluno_id}")
+# @app.get("/alunos/{aluno_id}",
+#         description='Consulta aluno por ID',
+#         summary='Salva a Ultima Consulta e "Cache"',
+#         response_description='Retorna Aluno consultado pelo ID'
+#         )
 # async def get_aluno(aluno_id:int):
 #     try:
 #         aluno = alunos[aluno_id]
@@ -34,12 +54,21 @@ async def get_alunos():
 
 # Evidencia o ID consultado no final (Cache) - Se a função acima estiver descomentada sobre-escreve essa aqui.
 # Ao consulta todos os alunos depois de consultar 1 ID aparece o ID consultado no final (No get)
-@app.get("/alunos/{aluno_id}")
-async def mostra_aluno_consultado(aluno_id:int = Path(default=None, title='ID Aluno', description='Deve ser entre 1 e 4', gt=0, lt=5)):
+@app.get("/alunos/{aluno_id}",
+        description='Consulta aluno por ID / Salva o Ultimo ID consultado em "Cache"',
+        summary='Consulta aluno por ID',
+        response_description='Consulta aluno pelo ID e apartir da 2a consulta salva o "Cache"'
+        # response_model=List[Aluno]
+        )
+async def mostra_aluno_consultado(aluno_id: int = Path(default=None, title='ID Aluno', description='eve ser entre 1 ou 2', gt=0, lt=3), db :Any =Depends(db)):
     try:
-        aluno = alunos[aluno_id]
-        alunos.update({"id":aluno_id})
-        return aluno
+        # aluno = alunos[aluno_id]
+        # print(alunos)
+        for aluno in alunos:
+            if aluno.id == aluno_id:
+                return aluno
+
+
     except KeyError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Aluno nao encontrado."
@@ -47,7 +76,12 @@ async def mostra_aluno_consultado(aluno_id:int = Path(default=None, title='ID Al
 
 
 # Salvar dados usando POST / Criar novo aluno
-@app.post("/alunos", status_code=status.HTTP_201_CREATED)
+@app.post("/alunos", 
+        status_code=status.HTTP_201_CREATED,
+        description='Salva/Cria uma novo aluno',
+        summary='Criar um novo Aluno',
+        response_description='Salva o novo aluno Cadastrado'
+        )
 async def post_aluno(aluno:Aluno):
     next_id : int = len(alunos) +1
     alunos[next_id] = aluno
@@ -55,7 +89,11 @@ async def post_aluno(aluno:Aluno):
     return aluno
 
 # Buscar aluno pelo nome
-@app.get("/alunosnomes/{aluno_nome}")
+@app.get("/alunosnomes/{aluno_nome}",
+        description='Buscar aluno pelo nome.',
+        summary='Busca aluno pelo nome',
+        response_description='Retorna o aluno consultado pelo nome'
+        )
 async def get_Nomealuno(aluno_nome:str):
     for aluno in alunos.values():
         if aluno['Nome'] == aluno_nome:
@@ -63,7 +101,11 @@ async def get_Nomealuno(aluno_nome:str):
             return aluno
 
 # Alterar dados do aluno, consultar o ID do aluno e alterar usando o PUT no Thunder
-@app.put("/alunos/{aluno_id}")
+@app.put("/alunos/{aluno_id}",
+        description='Alterar dados do aluno usando o ID e pegando infos do Body',
+        summary='Alterar dados do aluno pelo ID',
+        response_description='Retorna aluno com dados alterados'
+        )
 async def put_aluno(aluno_id: int, aluno:Aluno):
     if aluno_id in alunos:
         alunos[aluno_id] = aluno
@@ -75,22 +117,32 @@ async def put_aluno(aluno_id: int, aluno:Aluno):
         )
 
 # Deletar o aluno pelo ID
-@app.delete("/alunos/{aluno_id}")
+@app.delete("/alunos/{aluno_id}",
+        description='Deletar aluno pelo ID',
+        summary='Deletar aluno pelo ID',
+        response_description='Efetua o delete do aluno pelo ID'
+        )
 async def delete_aluno(aluno_id: int):
     if aluno_id in alunos:
         del alunos[aluno_id]
-        raise HTTPException(status_code=status.HTTP_200_OK, detail="Aluno deletado.")
+        # raise HTTPException(status_code=status.HTTP_200_OK, detail="Aluno deletado.")
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='Aluno nao encontrado.'
         )
 
-@app.get('/calculadora/')
-async def calcular(a: int, b: int, c: int):
-    soma = a + b + c
-    raise HTTPException(
-        status_code=status.HTTP_200_OK, detail=f"NRs - {a} + {b} + {c} = {soma}"
-    )
+@app.get('/calculadora/',
+        description='Calculadora usando Query, Optional e Header',
+        summary='Calculadora',
+        response_description='Retorna o resultado e o valor do Header"xdevs" caso tenha.')
+async def calcular(a: int = Query(default=None, gt=5), b: int = Query(default=None, gt=5), c: Optional[int] = None, xdevs: str =Header(default=None)):
+    soma = a + b
+    if c:
+        soma = soma + c
+    print(f'devs: {xdevs}')
+    return soma     
+
 
 if __name__ == '__main__':
     import uvicorn
--- FastAPI/Aplicacao/models.py
+++ FastAPI/Aplicacao/models.py
@@ -6,3 +6,10 @@ class Aluno(BaseModel):
     nome : str
     idade : int
     email : str
+
+alunos = [
+    Aluno(id=1, nome="Thiago", idade=37, email="Thiago@email.com"),
+    Aluno(id=2, nome="Joao", idade=17, email="joao@email.com"),
+    Aluno(id=3, nome="Vander", idade=41, email="vander@email.com"),
+    Aluno(id=4, nome="Jean", idade=42, email="jean@email.com")
+]
